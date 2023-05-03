e5084a6b42e4e83d28d32fcce7c308c66ce483de
Diego Reyes <diegorele13@gmail.com>
2023-05-03 13:39:03 -0500

    learn about alias

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,24 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
 	entry: "./src/index.js",
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: "main.js",
+		filename: "[name].[contenthash].js",
 		assetModuleFilename: "assets/images/[hash][ext][query]",
 	},
 	resolve: {
 		extensions: [".js"],
+		alias: {
+			"@utils": path.resolve(__dirname, "src/utils/"),
+			"@templates": path.resolve(__dirname, "src/templates/"),
+			"@styles": path.resolve(__dirname, "src/styles/"),
+			"@images": path.resolve(__dirname, "src/assets/"),
+		},
 	},
 	module: {
 		rules: [
@@ -41,9 +49,9 @@ module.exports = {
 					options: {
 						limit: 10000,
 						mimetype: "application/font-woff",
-						name: "[name].[ext]",
+						name: "[name].[contenthash].[ext]",
 						outputPath: "./assets/fonts/",
-						publicPath: "./assets/fonts/",
+						publicPath: "../assets/fonts/",
 						esModule: false,
 					},
 				},
@@ -56,7 +64,9 @@ module.exports = {
 			template: "./public/index.html",
 			filename: "./index.html",
 		}),
-		new MiniCssExtractPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "assets/[name].[contenthash].css",
+		}),
 		new CopyPlugin({
 			patterns: [
 				{
@@ -66,4 +76,8 @@ module.exports = {
 			],
 		}),
 	],
+	optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+	},
 };
